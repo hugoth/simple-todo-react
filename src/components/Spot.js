183824fe8b8563c7e60b11c7ab98ab9051569f57
Hugo Tessier <htessierhuort@gmail.com>
2019-05-07 17:44:25 +0200

    checking CORS policy

--- src/components/Spot.js
+++ src/components/Spot.js
@@ -10,11 +10,11 @@ const Spot = props => {
           stack = "line-through";
         }
         return (
-          <div key={spot.key} className="spotList">
+          <div key={spot._id} className="spotList">
             <DeleteButton onDelete={props.onClickDelete} value={spot} />
             <li
               className="spots"
-              onClick={() => props.onClickDone(id)}
+              onClick={() => props.onClickDone(spot._id, id)}
               style={{ textDecoration: stack }}
             >
               {spot.title}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "../index.css";
 import Spot from "./Spot";
 import Input from "./Input";
+import axios from "axios";
 
 class TodoList extends Component {
   state = {
@@ -17,6 +18,7 @@ class TodoList extends Component {
   };
 
   handleAddSpot = e => {
+    console.log(this.state.listSpot);
     const { title } = this.state.spot;
     const newTask = {
       title: title,
@@ -33,20 +35,53 @@ class TodoList extends Component {
       title: ""
     };
     this.setState({ spot: newspot });
+
+    axios
+      .post("https://todo-server-hth.herokuapp.com/create", {
+        title: this.state.spot.title,
+        isDone: false
+      })
+      .then(function(response) {
+        console.log(response.data.message);
+        this.setState({ message: response.data.message });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   };
 
-  handleDoneSpot = index => {
-    console.log(index);
+  handleDoneSpot = (id, index) => {
+    axios
+      .post("https://todo-server-hth.herokuapp.com/update", {
+        id: id
+      })
+      .then(function(response) {
+        console.log(response.data.message);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
     const { listSpot } = this.state;
     const newList = [...listSpot];
     newList[index] = { ...newList[index] };
-
     newList[index].isDone = !newList[index].isDone;
-    console.log(newList[index].isDone);
     this.setState({ listSpot: newList });
   };
 
   handleDelete = selectedSpot => {
+    const id = selectedSpot._id;
+    axios
+      .post("hhttps://todo-server-hth.herokuapp.com/delete", {
+        id: id
+      })
+      .then(function(response) {
+        console.log(response.data.message);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
     // pourquoi ne pas faire de copie fonctionne et faire une copie ne fonctionne pas ?
     const { listSpot } = this.state;
     const newList = listSpot.filter(spot => {
@@ -75,8 +110,16 @@ class TodoList extends Component {
           onSubmit={this.handleAddSpot}
           handleDeleteAllTasks={this.handleDeleteAllTasks}
         />
+        {this.state.message}
       </div>
     );
+  }
+  async componentDidMount() {
+    const response = await axios.get("https://todo-server-hth.herokuapp.com/");
+    const spots = response.data;
+    this.setState({
+      listSpot: spots
+    });
   }
 }
 
